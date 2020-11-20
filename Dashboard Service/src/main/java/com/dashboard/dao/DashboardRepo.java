58083885eced45d6fc52db37b7b46c2f384364db
Zeddling <victorzeddys@gmail.com>
2020-11-20 20:07:03 +0300

    Diagnosis service added and small changes to other services

--- Dashboard Service/src/main/java/com/dashboard/dao/DashboardRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dashboard.dao;
-
-import com.dashboard.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DashboardRepo extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
-}
